0e2a4d43b4b17bb0a3d0053531034bf9e0190667
Bagel™ <djmiller03@twcny.rr.com>
2022-04-23 11:35:17 -0400

    Update weapon_pvb_laevatein.lua

--- entities/weapons/weapon_pvb_laevatein.lua
+++ entities/weapons/weapon_pvb_laevatein.lua
@@ -62,10 +62,10 @@ SWEP.Primary.Ammo			= "pistol"					-- What kind of ammo.  Options, besides custo
 SWEP.DisableChambering = true --Disable round-in-the-chamber
 
 --Recoil Related
-SWEP.Primary.KickUp			= 0.10				-- This is the maximum upwards recoil (rise)
-SWEP.Primary.KickDown			= 0.08					-- This is the maximum downwards recoil (skeet)
-SWEP.Primary.KickHorizontal			= 0.05					-- This is the maximum sideways recoil (no real term)
-SWEP.Primary.StaticRecoilFactor = 0.25 	--Amount of recoil to directly apply to EyeAngles.  Enter what fraction or percentage (in decimal form) you want.  This is also affected by a convar that defaults to 0.5.
+SWEP.Primary.KickUp					= 0				-- This is the maximum upwards recoil (rise)
+SWEP.Primary.KickDown				= 0					-- This is the maximum downwards recoil (skeet)
+SWEP.Primary.KickHorizontal			= 0					-- This is the maximum sideways recoil (no real term)
+SWEP.Primary.StaticRecoilFactor		= 0 	--Amount of recoil to directly apply to EyeAngles.  Enter what fraction or percentage (in decimal form) you want.  This is also affected by a convar that defaults to 0.5.
 
 --Firing Cone Related
 
