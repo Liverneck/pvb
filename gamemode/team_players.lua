39516f9fdf35c52ea35f55bd8dfcb368fc933f5c
Bagel™ <djmiller03@twcny.rr.com>
2022-04-23 11:35:29 -0400

    Edit values

--- gamemode/team_players.lua
+++ gamemode/team_players.lua
@@ -4,7 +4,7 @@ local PLAYER = {}
 
 PLAYER.DisplayName			= "Player"
 PLAYER.WalkSpeed 			= 260
-PLAYER.RunSpeed				= 310
+PLAYER.RunSpeed				= 300
 PLAYER.CrouchedWalkSpeed 	= 0.4
 PLAYER.DuckSpeed			= 0.3
 PLAYER.UnDuckSpeed			= 0.3
@@ -13,7 +13,7 @@ PLAYER.CanUseFlashlight     = false
 PLAYER.MaxHealth			= 100
 PLAYER.StartHealth			= 100
 PLAYER.StartArmor			= 0
-PLAYER.DropWeaponOnDie		= true
+PLAYER.DropWeaponOnDie		= false
 PLAYER.AvoidPlayers			= false
 
 player_manager.RegisterClass( "class_player", PLAYER, "player_default" )
